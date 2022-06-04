9434191635a8242dddba5757dd919a947376913c
Asadujjaman Julhas <fahim0760@gmail.com>
2022-06-04 23:07:59 +0600

    added fake data

---
+++ fakeData.js
@@ -0,0 +1,39 @@
+const userList = [
+  {
+    "id": 1,
+    "name": "John",
+    "userName": "Done",
+    age: 20,
+    "nationality": "USA"
+  },
+  {
+    "id": 2,
+    "name": "Asadujjaman",
+    "userName": "asadjulhas",
+    age: 27,
+    "nationality": "BD"
+  },
+  {
+    "id": 3,
+    "name": "Jenifer",
+    "userName": "jane",
+    age: 24,
+    "nationality": "Canada"
+  },
+  {
+    "id": 4,
+    "name": "Fazi Mim",
+    "userName": "fazimim",
+    age: 22,
+    "nationality": "Bangladesh"
+  },
+  {
+    "id": 6,
+    "name": "David",
+    "userName": "db",
+    age: 35,
+    "nationality": "Brazil"
+  },
+];
+
+module.exports = { userList }
